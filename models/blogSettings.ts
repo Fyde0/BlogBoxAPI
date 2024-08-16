7a83053d1f37417274c3ba7ca36b6b39d5b82d61
Federico <fyde90@gmail.com>
2024-08-16 13:47:06 +0200

    added featured posts in blog settings

--- models/blogSettings.ts
+++ models/blogSettings.ts
@@ -8,7 +8,9 @@ const BlogSettingsSchema: Schema = new Schema<IBlogSettings>(
         title: { type: String },
         theme: { type: String, enum: ["minty", "flatly", "cosmo"], default: "minty" },
         homeLayout: {
-            postPreviewStyle: {type: String, enum: postPreviewLgStyles, default: "LgDefault"}
+            postPreviewStyle: {type: String, enum: postPreviewLgStyles, default: "LgDefault"},
+            featuredPosts: {type: Boolean, default: false},
+            featuredPostsTags: { type: [String] }
         }
     },
     {
